88736bc60c41d235cfe582596beb15209109572d
NexTPL <aleksander.lyskawa1@gmail.com>
2021-09-28 20:44:06 +0200

    App.js and CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header></header>
+			<div class='players'>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+				<div class='player'></div>
+			</div>
+			<div className='timer'>
+				<p>Timer</p>
+			</div>
+			<footer></footer>
+		</div>
+	);
 }
 
 export default App;
